3967ae693b95a9368acd27a8a3eaa90e94539a6e
ThomasBeWeb <tcantinelli@gmail.com>
2018-05-07 16:03:07 +0200

    Verion avec pages differentes

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
+<?php
+session_start();
 
-<!DOCTYPE html>
-<html>
+//if(!($_SESSION)){
+//    header("Location: contents/connexion.php");
+//}
 
-    <head>
-        <title>Restaurant</title>
-        <meta charset="UTF-8">
-        <link rel="icon" type="image/png" href="./favicon.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-        <link href="./style.css" rel="stylesheet">
-    </head>
-
-    <body>
-        <div class="container">
-            <?php include "./template/header.php";?>
-        </div>
-       
-    </body>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.11/css/all.css" integrity="sha384-p2jx59pefphTFIpeqCcISO9MdVfIm4pNnsL08A6v5vaQc4owkQqxMV8kg4Yvhaw/" crossorigin="anonymous">
-</html>		
-			
+include "./template/header.html";
+include "./template/navigation.php";
+include "./template/footer.html";
+?>		
